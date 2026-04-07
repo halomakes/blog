df74ff6b2e8a6a9d06fd6d4e598d0a20263ddb8f
halo <me@haley.contact>
2026-04-07 09:54:03 -0400

    scaffold new project

---
+++ Halomakes.Cms/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Halomakes.Cms.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
