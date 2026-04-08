18a1dfda413c8cb83828803c12ec9a7111e9b124
halo <me@haley.contact>
2026-04-08 18:24:41 -0400

    adjust particle density to screen size

--- Halomakes.Blog/theme/src/particles.ts
+++ Halomakes.Blog/theme/src/particles.ts
@@ -1,19 +1,28 @@
-import {tsParticles} from "@tsparticles/engine";
+import {tsParticles, type Container} from "@tsparticles/engine";
 import {loadTrianglesPreset} from "@tsparticles/preset-triangles"
+
+declare let window: { particles?: Container };
 
 (async () => {
     await loadTrianglesPreset(tsParticles);
 
-    await tsParticles.load({
+    window.particles = await tsParticles.load({
         id: "tsparticles",
         options: {
             preset: "triangles",
             particles: {
                 move: {
                     speed: {min: .003, max: .2}
+                },
+                number: {
+                    density: {
+                        enable: true,
+                        width: 1920,
+                        height: 1080
+                    }
                 }
             },
-            fpsLimit: 30
+            fpsLimit: 60
         },
     });
 })();
