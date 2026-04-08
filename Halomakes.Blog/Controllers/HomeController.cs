61e55c664307733e355ea1b116b7814fb162b128
halo <me@haley.contact>
2026-04-07 21:20:40 -0400

    reroute home

--- Halomakes.Blog/Controllers/HomeController.cs
+++ Halomakes.Blog/Controllers/HomeController.cs
@@ -8,12 +8,7 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
+        return RedirectToAction(nameof(PostsController.GetRecentPosts), "Posts");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
